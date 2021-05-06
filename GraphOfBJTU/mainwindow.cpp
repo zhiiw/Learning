77f90947d6277ec39952b001e6b61b245f4ea032
zhiiw <1302593554@qq.com>
2021-05-06 22:36:17 +0800

    graph

--- GraphOfBJTU/mainwindow.cpp
+++ GraphOfBJTU/mainwindow.cpp
@@ -1,12 +1,28 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+        : QMainWindow(parent)
+        , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    db = new sqlUse();
+    if(db->db.open()){
+        cout<<"open successfully"<<endl;
+    }
+    QSqlQuery query;
+    query.exec("SELECT name, salary FROM employee WHERE salary > 50000");
+    while (query.next()) {
+        QString name = query.value(0).toString();
+        int salary = query.value(1).toInt();
+        rp->AddEdge()
+    }
+    string str = db->db.exec("select * from ");
+    for{
+
+    }
+
 }
 
 MainWindow::~MainWindow()
@@ -14,3 +30,9 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->lineEdit->text();
+
+}
